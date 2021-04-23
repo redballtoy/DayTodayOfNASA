969ddfcd81684a7ef94faf6267932b4a3629782a
Dmitry Sorokin <redballtoy@gmail.com>
2021-04-23 23:24:39 +0300

    lection_02 change theme

--- app/src/main/java/com/example/redballtoy/daytodayofnasa/MainActivity.kt
+++ app/src/main/java/com/example/redballtoy/daytodayofnasa/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.example.redballtoy.daytodayofnasa
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.redballtoy.daytodayofnasa.view.PictureOfTheDayFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //set theme
+        val sharedPreferences = getSharedPreferences("Theme", Context.MODE_PRIVATE)
+        val themeName = sharedPreferences.getString("ThemeName","Light")
+        when(themeName){
+            "Light" ->  setTheme(R.style.Theme_DayTodayOfNASA)
+            "Dark" ->  setTheme(R.style.Theme_DayTodayOfNASA)
+            "Indigo" ->  setTheme(R.style.IndigoTheme)
+            "Pink" ->  setTheme(R.style.PinkTheme)
+        }
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
